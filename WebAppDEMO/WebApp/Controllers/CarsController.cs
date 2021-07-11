62d0135fc9c6afd0501c0e8d720081e0796c6a34
Desyslava-gs <84912601+Desyslava-gs@users.noreply.github.com>
2021-07-11 20:41:24 +0300

    Test 2 Commit

--- WebAppDEMO/WebApp/Controllers/CarsController.cs
+++ WebAppDEMO/WebApp/Controllers/CarsController.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebApp.Models.Cars;
 
 namespace WebApp.Controllers
 {
     public class CarsController : Controller
     {
-        public IActionResult Cars()
+        public IActionResult Add()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Add(AddCarFormModel car)
+        {
+            return View();
+        }
+
     }
-}
+} 
