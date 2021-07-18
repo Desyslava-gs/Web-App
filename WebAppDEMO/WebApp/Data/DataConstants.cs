c07134a455172c0aaa9628a4ecc97be5f6c61dd3
Desyslava-gs <84912601+Desyslava-gs@users.noreply.github.com>
2021-07-18 19:13:43 +0300

    Add Validate For CreateCarFormModel

--- WebAppDEMO/WebApp/Data/DataConstants.cs
+++ WebAppDEMO/WebApp/Data/DataConstants.cs
@@ -12,11 +12,16 @@ namespace WebApp.Data
         public const int CarMakeMaxLength = 20;
         public const int CarModelMaxLength = 30;
         public const int CarYearMinValue = 1900;
-        public const int CarYearMaxValue =2100;
+        public const int CarYearMaxValue = 2100;
 
-        public const int CarFuelKindMaxLength= 10;
         public const int CarPlateNumberMaxLength = 8;
         public const int CarVinNumberMaxLength = 50;
         public const int CarColorMaxLength = 10;
+
+        public const int CarMakeMinLength = 2;
+        public const int CarModelMinLength = 2;
+        public const int CarColorMinLength = 3;
+        public const int CarVinNumberMinLength = 6;
+        public const int CarDescriptionMinLength = 4;
     }
 }
--- WebAppDEMO/WebApp/Models/Cars/CreateCarFormModel.cs
+++ WebAppDEMO/WebApp/Models/Cars/CreateCarFormModel.cs
@@ -4,37 +4,54 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using WebApp.Data.Models;
+using WebApp.Infrastructure;
 using static WebApp.Data.DataConstants;
 namespace WebApp.Models.Cars
 {
     public class CreateCarFormModel
-    {  
+    {
         //[Key]
         //[Required]
         //public string Id { get; set; }
 
-        
+
         [Required]
-        [MaxLength(CarMakeMaxLength)]
+        [StringLength(CarMakeMaxLength, MinimumLength = CarMakeMinLength)]
         public string Make { get; set; }
 
         [Required]
-        [MaxLength(CarModelMaxLength)]
-        public string Model { get; set; } 
+        [StringLength(CarModelMaxLength, MinimumLength = CarModelMinLength)]
+        public string Model { get; set; }
 
+
+        [Range(CarYearMinValue, CarYearMaxValue)]
         public int Year { get; set; }
-        
+        [Required]
+        [StringLength(CarColorMaxLength, MinimumLength = CarColorMinLength)]
         public string Color { get; set; }
-        
+
+        [Required]
+        [MaxLength(CarPlateNumberMaxLength)]
+        [RegularExpression("^[A-Z]{2}[0-9]{4}[A-Z]{2}$")]
         public string PlateNumber { get; set; }
 
+        [Required]
+        [StringLength(CarVinNumberMaxLength, MinimumLength = CarVinNumberMinLength)]
         public string VinNumber { get; set; }
-        
-        public string PictureUrl { get; set; }
 
+        [Required]
+        [Url]
+        public string PictureUrl { get; set; }
+        [Required]
+        [StringLength(
+            int.MaxValue, 
+            MinimumLength = CarDescriptionMinLength, 
+            ErrorMessage = "The field Description must be minimum length of {2}.")]
         public string Description { get; set; }
-        [Display (Name = "FuelType")]
+
+      //  [Display(Name = "Fuel Type")]
         public string FuelTypeId { get; set; }
+        [Required]
         public IEnumerable<FuelTypeViewModel> FuelTypes { get; set; }
     }
 }
