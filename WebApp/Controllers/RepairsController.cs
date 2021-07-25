d0f4ffb9c679b843947cdde01eafc3919e72d42b
Desyslava-gs <84912601+Desyslava-gs@users.noreply.github.com>
2021-07-26 01:33:15 +0300

    Git

---
+++ WebApp/Controllers/RepairsController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Data;
+using WebApp.Data.Models;
+
+namespace WebApp.Controllers
+{
+    public class RepairsController : Controller
+    {
+        private readonly CarRepairDbContext _context;
+
+        public RepairsController(CarRepairDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Repairs
+        public async Task<IActionResult> Index()
+        {
+            var carRepairDbContext = _context.Repairs.Include(r => r.Car).Include(r => r.RepairType);
+            return View(await carRepairDbContext.ToListAsync());
+        }
+
+        // GET: Repairs/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var repair = await _context.Repairs
+                .Include(r => r.Car)
+                .Include(r => r.RepairType)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (repair == null)
+            {
+                return NotFound();
+            }
+
+            return View(repair);
+        }
+
+        // GET: Repairs/Create
+        public IActionResult Create()
+        {
+            ViewData["CarId"] = new SelectList(_context.Cars, "Id", "Id");
+            ViewData["RepairTypeId"] = new SelectList(_context.RepairTypes, "Id", "Id");
+            return View();
+        }
+
+        // POST: Repairs/Create
+      
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Price,StartDate,EndDate,Description,CarId,RepairTypeId")] Repair repair)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(repair);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CarId"] = new SelectList(_context.Cars, "Id", "Id", repair.CarId);
+            ViewData["RepairTypeId"] = new SelectList(_context.RepairTypes, "Id", "Id", repair.RepairTypeId);
+            return View(repair);
+        }
+
+        // GET: Repairs/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var repair = await _context.Repairs.FindAsync(id);
+            if (repair == null)
+            {
+                return NotFound();
+            }
+            ViewData["CarId"] = new SelectList(_context.Cars, "Id", "Id", repair.CarId);
+            ViewData["RepairTypeId"] = new SelectList(_context.RepairTypes, "Id", "Id", repair.RepairTypeId);
+            return View(repair);
+        }
+
+        // POST: Repairs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,Name,Price,StartDate,EndDate,Description,CarId,RepairTypeId")] Repair repair)
+        {
+            if (id != repair.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(repair);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RepairExists(repair.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CarId"] = new SelectList(_context.Cars, "Id", "Id", repair.CarId);
+            ViewData["RepairTypeId"] = new SelectList(_context.RepairTypes, "Id", "Id", repair.RepairTypeId);
+            return View(repair);
+        }
+
+        // GET: Repairs/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var repair = await _context.Repairs
+                .Include(r => r.Car)
+                .Include(r => r.RepairType)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (repair == null)
+            {
+                return NotFound();
+            }
+
+            return View(repair);
+        }
+
+        // POST: Repairs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var repair = await _context.Repairs.FindAsync(id);
+            _context.Repairs.Remove(repair);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RepairExists(string id)
+        {
+            return _context.Repairs.Any(e => e.Id == id);
+        }
+    }
+}
